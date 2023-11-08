617931c78eb6c61e51e70b4b661bdbd7d4ba5426
kzk4043 <okazakishoma@team-lab.com>
2023-11-09 08:29:18 +0900

    feat: index.astro

--- src/config.ts
+++ src/config.ts
@@ -9,37 +9,41 @@ export const SITE = {
 
 export const OPEN_GRAPH = {
   image: {
-    src: "/kzk4043-logo.png",
+    src: "/images/kzk4043-logo.png",
     alt: "kzk4043 logo",
   },
   twitter: "",
 };
 
+/**
+ * header: true であれば、トップにカード表示される
+ */
 export const SIDEBAR = [
-  { text: "Core", header: true },
+  // TODO: 余分なリンク消す
+  // { text: "Core", header: true },
   { text: "Introduction", link: "/core/introduction" },
-  { text: "Colors", link: "/core/colors" },
-  { text: "Typography", link: "/core/typography" },
-  { text: "Shadows", link: "/core/shadows" },
+  // { text: "Colors", link: "/core/colors" },
+  // { text: "Typography", link: "/core/typography" },
+  // { text: "Shadows", link: "/core/shadows" },
 
-  { text: "Components", header: true },
-  { text: "Buttons", link: "/components/buttons" },
-  { text: "Input", link: "/components/input" },
-  { text: "Status pills", link: "/components/status-pill" },
-  { text: "Table", link: "/components/table" },
+  // { text: "Components", header: true },
+  // { text: "Buttons", link: "/components/buttons" },
+  // { text: "Input", link: "/components/input" },
+  // { text: "Status pills", link: "/components/status-pill" },
+  // { text: "Table", link: "/components/table" },
 
-  { text: "Patterns", header: true },
-  { text: "Introduction", link: "/patterns/introduction" },
+  // { text: "Patterns", header: true },
+  // { text: "Introduction", link: "/patterns/introduction" },
 
   // 全体/一般
-  { text: "全体/一般", header: true },
+  { text: "全体/一般", header: true, description: "勉強法、仕事術、採用など", url: "/general/introduction", image: "/images/general.png" },
   { text: "Introduction", link: "/general/introduction" },
 
   // プロジェクトマネジメント
-  { text: "プロジェクトマネジメント", header: true },
+  { text: "プロジェクトマネジメント", header: true, description: "プロジェクトの進め方", url: "/project-management/introduction", image: "/images/project-management.png" },
   { text: "Introduction", link: "/project-management/introduction" },
 
   // フロントエンド
-  { text: "フロントエンド", header: true },
+  { text: "フロントエンド", header: true, description: "フロントエンドの環境構築、抑えておきたい技術についてなど", url: "/frontend/introduction", image: "/images/frontend.png" },
   { text: "Introduction", link: "/frontend/introduction" },
 ];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Fira Code", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
