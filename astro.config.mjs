d3f8d9d4244cbc56a934bc2dbecaccb1e04eb930
kzk4043 <okazakishoma@team-lab.com>
2023-11-15 10:14:22 +0900

    feat: delete unnecessary files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  vue(), tailwind(), mdx()]
+    // Enable Preact to support Preact JSX components.
+    tailwind(),
+    mdx(),
+  ],
 });
--- src/pages/patterns/introduction.md
+++
@@ -1,9 +0,0 @@
----
-title: patterns
-pubDate: "2023/11/15"
-layout: ../../layouts/MainLayout.astro
----
-
-# Patterns
-
-Common patterns for building interfaces.
